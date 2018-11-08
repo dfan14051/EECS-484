5b501530a4369dfb7ee544f5987de5e6dac0ddd4
David Fan <dfan14051@gmail.com>
2018-11-07 22:30:08 -0500

    merp

--- ps5_CAM/updateRandomNode.m
+++ ps5_CAM/updateRandomNode.m
@@ -1,5 +1,12 @@
    % fnc to correct nodes in matrix memory
    %YOU MUST WRITE THIS FUNCTION
    function [vec,delta] = updateRandomNode(MatrixMem,vec)
-     delta=0; %count how many nodes change their values
+     idx = randi(size(vec, 1));
+     if vec(idx) ~= sign(MatrixMem(idx,:) * vec)
+         delta=1;
+         vec(idx) = sign(MatrixMem(idx,:) * vec);
+     else
+        delta=0; %count how many nodes change their values
+     end
+     
     
